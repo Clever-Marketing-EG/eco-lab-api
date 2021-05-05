b116f406b53bbf9d8802ff44074418ca9f860dfc
Raymond Youssef <rimonomega@gmail.com>
2021-05-05 14:10:13 +0200

    Initial commit

---
+++ database/migrations/2021_05_05_120030_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_ar');
+            $table->string('description');
+            $table->string('description_ar');
+            $table->text('points');
+            $table->text('points_ar');
+            $table->bigInteger('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
