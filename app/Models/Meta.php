b6b874f1d57a62cf98e961d25be797d886fa6e3e
Rana <rana.medhat69@gmail.com>
2021-05-08 14:40:33 +0200

    Added Meta Controller

---
+++ app/Models/Meta.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\http\Request;
+
+class Meta extends Model {
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public $timestamps = false;
+
+    /**
+    *
+    *
+    * @param Request $request
+    * @return array
+    */
+    public static function validate( Request $request ):array {
+
+        return $request->validate( [
+            'content' => 'required|string|min:3',
+            'content_ar' => 'required|string|min:3'
+        ] );
+
+    }
+
+    /**
+    * Load English Meta from the database
+    *
+    * @return mixed
+    */
+    public static function loadEnglish() {
+        return Meta::select( 'id', 'name', 'content', 'type', 'page' )
+        ->get()
+        ->toArray();
+
+    }
+
+    /**
+    * Load Arabic Meta from the database
+    *
+    * @return mixed
+    */
+    public static function loadArabic() {
+        return Meta::select( 'id', 'name', 'content_ar as content', 'type', 'page' )
+        ->get()
+        ->toArray();
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\MetaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,8 +11,17 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the 'api' middleware group. Enjoy building your API!
 |
 */
 
-Route::apiResource('products', ProductController::class);
+Route::apiResource( 'products', ProductController::class );
+
+/*
+|--------------------------------------------------------------------------
+| Meta Routes
+|--------------------------------------------------------------------------
+*/
+Route::apiResource( 'meta', MetaController::class )->except(['store', 'destroy']);
+Route::get('dashboard/meta',[MetaController::class, 'fullIndex']);
+
